d62a5913c626274f3b24c8a097c2e49fa5b5ce29
furetur <furetur@gmail.com>
2021-10-03 03:22:40 +0300

    events repository

---
+++ app/src/main/java/com/example/legionaryapp/data/EventsRepository.kt
@@ -0,0 +1,26 @@
+package com.example.legionaryapp.data
+
+import kotlinx.coroutines.delay
+
+object EventsRepository {
+    var events: List<Event> = emptyList()
+        private set
+
+    suspend fun fetchEvents() {
+        delay(100)
+        events = listOf(Event(id = 0, title = "Yeah", description = "Nice"))
+    }
+
+    suspend fun postEvent(title: String, description: String) {
+        delay(100)
+        events = events + listOf(
+            Event(
+                id = events.size,
+                title = "Yeah ${events.size}",
+                description = "Nice"
+            )
+        )
+    }
+}
+
+data class Event(val id: Int, val title: String, val description: String)
